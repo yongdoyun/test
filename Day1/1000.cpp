4f69da29f8fe6aa6451be7eadfd27c72d2304ae9
Yongdo Yun <yongdo@MacBookProo.w.mifi>
2021-01-13 16:28:27 +0900

    1

---
+++ Day1/1000.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	
+	int x, y;
+	cin >> x >> y;
+	cout << x+y << endl;
+	return 0;
+}
---
+++ Day1/10869.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	
+	int x, y;
+	cin >> x >> y;
+	cout << x + y << endl;
+	cout << x - y << endl;
+	cout << x * y << endl;
+	cout << x / y << endl;
+	cout << x % y << endl;
+
+	return 0;
+}
